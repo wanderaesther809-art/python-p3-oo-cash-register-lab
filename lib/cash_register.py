64cea12c5e0567bf180391538f747fe6a3f83f80
Esther Wandera <wanderaesther809@gmail.com>
2025-12-08 00:41:34 +0300

    initial commit

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0.0
+        self.discount = discount
+        self.items = []
+        self.last_transaction = 0.0
+
+    def add_item(self, title, price, quantity=1):
+        """Add item(s) to the register."""
+        self.last_transaction = price * quantity
+        self.total += self.last_transaction
+        self.items.extend([title] * quantity)
+
+    def apply_discount(self):
+        """Apply discount if it exists and print message."""
+        if self.discount > 0:
+            self.total -= (self.discount / 100) * self.total
+            print(f"After the discount, the total comes to ${int(self.total)}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        """Remove the last transaction from the total."""
+        self.total -= self.last_transaction
+        self.last_transaction = 0.0
